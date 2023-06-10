3c466b3dd81a45dfba40c4cd7c9b9b3886268c92
mrnikak <136177197+mrnikak@users.noreply.github.com>
2023-06-10 18:21:06 +0400

    Add files via upload

---
+++ styles/main.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("button");
+  const post = document.querySelector(".post");
+  const widget = document.querySelector(".star-widget");
+  const editbtn = document.querySelector(".edit");
+
+  btn.onclick = ()=>{
+    widget.style.display = "none";
+    post.style.display = "block";
+  editbtn.onclick = ()=>{
+    widget.style.display = "block";
+    post.style.display = "none";
+    return false;
+   }
+   return false;
+  }
+
